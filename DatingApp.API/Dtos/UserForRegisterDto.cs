0459300247f60158037bf2a8fce33ba519cae462
unknown <wisniak17@gmail.com>
2018-10-26 13:59:57 +0200

    Security

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(8,MinimumLength=4,ErrorMessage="Hasło musi zawierać 4-8 znaków")]
+        public string Password { get; set; }
+    }
+}
